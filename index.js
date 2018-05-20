9a3a680851111945eb1dc8bb4a1f1c775b3ab248
Mykhailo Panas <lv211184pmi@gmail.com>
2018-05-20 11:27:46 +0300

    added other REST API methods made them separate as routes

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
 const Joi = require('joi');
 const express = require('express');
 const app = express();
+const genres = require('./routes/genres');
 
 app.use(express.json());
-
-const genres = [
-    {id: 1, name: "commedy"},
-    {id: 2, name: "fantasy"},
-    {id: 3, name: "action"},
-    {id: 4, name: "detective"}
-];
-
-app.get('/api/genres', (req, res) => {
-    if(!genres) return res.status(404).send('resource not found');
-    res.send(genres);
-});
-
-app.post('api/genres', (req, res) => {
-    const { error } = validateGenre(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    };
-
-    genres.push(genre);
-    res.send(genre);
-});
-
+app.use('./api/genres', genres);
 
 const port = process.env.port || 3000;
-
 app.listen(3000, () => console.log(`Listening on port ${port}...`));
-
-function validateGenre(genre) {
-    const schema = {
-        name: Joi.string().min(5).required()
-    };
-    
-    return result = Joi.validate(course, schema);
-}
